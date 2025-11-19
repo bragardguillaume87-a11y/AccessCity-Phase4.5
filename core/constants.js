87f8a0bdb1383224ef16e6d7a03f8c4ae23a78c1
bragardguillaume87-a11y <bragard.guillaume87@gmail.com>
2025-11-19 02:19:44 +0100

    Add constants for scene and dialogue limits

---
+++ core/constants.js
@@ -0,0 +1,6 @@
+// === Fichier : core/constants.js ===
+export const MAX_SCENE_NAME_LENGTH = 200;
+export const MAX_DIALOGUE_LENGTH = 1000;
+export const MAX_CHOICE_TEXT_LENGTH = 300;
+export const SUPPORTED_EXPRESSIONS = ['happy', 'neutral', 'sad', 'angry', 'confused'];
+export const PATCH_OPERATORS = ['add', 'remove', 'replace', 'move', 'copy', 'test'];
